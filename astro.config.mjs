0e304514427d20102351af1d337fefecf59587c3
Bruno Donatelli <42386513+brunoredes@users.noreply.github.com>
2026-07-06 11:37:35 -0300

    Set up Astro MDX content collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
+import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://donatelli.dev',
-  integrations: [sitemap()],
-  output: 'static',
+	site: 'https://donatelli.dev',
+	integrations: [mdx(), sitemap()],
+	output: 'static',
 });
